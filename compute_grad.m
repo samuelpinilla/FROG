b31fe6208b235c1704bac9e206bc42fe022deb32
samuelpinilla <47015971+samuelpinilla@users.noreply.github.com>
2019-02-04 17:23:44 -0500

    Add files via upload

---
+++ compute_grad.m
@@ -0,0 +1,18 @@
+function grad = compute_grad(z, u, y, Params,i)
+
+    L      = Params.L;
+    N      = Params.n1;
+    batch  = Params.B;
+    
+    r      = batch/N;
+    rc     = ceil(i/N);
+    fi     = rc:rc+r-1;
+    
+    zf     = aux(z,L,N,fi);
+    
+    ysub = y(:,fi);
+    
+    Az   = fftshift(fft(FROG_signal_B(z,L,N,fi)),1);
+    yz   = sqrt(abs(Az).^2+u^2);
+    grad = 4*sum(conj(zf).*ifft(fftshift(((yz-ysub)./yz).*Az,1)),2)*N/batch;
+end
