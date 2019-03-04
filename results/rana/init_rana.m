9ec964acddb67661fc48ce977bbd861d08bee4db
samuelpinilla <47015971+samuelpinilla@users.noreply.github.com>
2019-03-04 14:26:21 -0500

    Update init_rana.m

--- results/rana/init_rana.m
+++ results/rana/init_rana.m
@@ -1,75 +1,21 @@
-function x1 = init_rana(Y,x)
-%% initialization
-N  = size(Y,1);
-g  = sum(Y,2);
-
-s = sqrt(ifft(ifftshift(g)));
-s = s/norm(s,'fro');
-
-%% variables
-alpha = 0.09;
-beta  = 0.425;
-gamma = 1;
-
-t0 = randsrc(1,1,3:N-3);
-fprintf('t0=%d\n',t0);
-ll = t0+1:1:N;
-
-for r=1:length(ll)
-    t = ll(r);
-    
-    term1p = alpha*abs(s(t)-s(t-1))^2;
-    term1n = alpha*abs(-s(t)-s(t-1))^2;
-    
-    term2p = beta*abs(s(t)-s(t-1)-(s(t-1)-s(t-2)))^2;
-    term2n = beta*abs(-s(t)-s(t-1)-(s(t-1)-s(t-2)))^2;
-        
-    term3p = gamma*abs(s(t)-s(t-1)-(s(t-1)-s(t-2)) - (s(t-1)-s(t-2)-(s(t-2)-s(t-3))))^2;
-    term3n = gamma*abs(-s(t)-s(t-1)-(s(t-1)-s(t-2)) - (s(t-1)-s(t-2)-(s(t-2)-s(t-3))))^2;
-    
-    epsilon1 = term1p + term2p + term3p;
-    epsilon2 = term1n + term2n + term3n;
-    
-    if epsilon1 > epsilon2
-        s(t) = -s(t);
-    end
+function x = init_rana(Isig,filter)
+addpath(genpath('Subcodes'));
+N  = size(Isig,1);
+if filter
+    Isig = super_gaussian(Isig,2,2,2);
+    fI_shg = ifft_FROG(Isig);
+    fI_shg = super_gaussian(fI_shg,100,.75,10);
+    Isig = abs(fft_FROG(fI_shg));
+    fI_shg = fftc(Isig, [], 2);
+    fI_shg = super_gaussian(fI_shg,.75, 100,10);
+    Isig = abs(ifftc(fI_shg,[],2));
+    Isig  = remove_neg(Isig);
 end
 
-ll = t0-1:-1:1;
+Isig = frogbacksub_noise(Isig, [5 5],[5 5]);
+Mwin = sum(Isig,2);                                     %frequency marginal
 
-for r=1:length(ll)
-    t = ll(r);
-    
-    term1p = alpha*abs(s(t)-s(t+1))^2;
-    term1n = alpha*abs(-s(t)-s(t+1))^2;
-    
-    term2p = beta*abs(s(t)-s(t+1)-(s(t+1)-s(t+2)))^2;
-    term2n = beta*abs(-s(t)-s(t+1)-(s(t+1)-s(t+2)))^2;
-        
-    term3p = gamma*abs(s(t)-s(t+1)-(s(t+1)-s(t+2)) - (s(t+1)-s(t+2)-(s(t+2)-s(t+3))))^2;
-    term3n = gamma*abs(-s(t)-s(t+1)-(s(t+1)-s(t+2)) - (s(t+1)-s(t+2)-(s(t+2)-s(t+3))))^2;
-        
-    epsilon1 = term1p + term2p + term3p;
-    epsilon2 = term1n + term2n + term3n;
-    
-    if epsilon1 > epsilon2
-        s(t) = -s(t);
-    end
-end
-
-%% estimated spectrum
-
-x0 = sqrt(fftshift(fft(s)/sqrt(N))).*exp(-1i*randsrc(N,1,[0,1]));
-% x0 = x0/norm(x0);
-
-%% real spectrum
-% Re = fftshift(abs(fft(x)))/sqrt(N);
-
-% error = norm(abs(x0)-Re)/N;
-% fprintf('spectrum estimation: %f\n',error);
-
-% figure;plot(1:N,abs(x0)/norm(x0),1:N,Re);
-
-x1 = ifft(ifftshift(x0))*sqrt(N);
-x1 = x1/norm(x1,'fro');
+Sout = SHG_marginal(Mwin, 1, 1) ;           % getting only one spectrum
+x    = ifft(ifftshift(sqrt(Sout).*exp(1i*rand(1,N))));
+x    = x.';
 end
