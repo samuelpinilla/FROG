5e62a6e8914ea261091355b5ea9ed79044a1b106
samuelpinilla <47015971+samuelpinilla@users.noreply.github.com>
2019-03-08 18:32:10 -0500

    Update results.m

--- results/results.m
+++ results/results.m
@@ -1,10 +1,8 @@
-clc;
-clear;
-close all;
 clear classes;
 
 addpath(genpath('smoothing'));
 addpath(genpath('ptych'));
+addpath(genpath('rana'));
 addpath(genpath('pcpg'));
 
 load('pulse_set.mat');
@@ -17,12 +15,12 @@ figure;
 for t = 1:length(pp)
     x = pulse_set(pp(t),:).';
     [z_s,~,~,~] = smoothing_solver(x,[],1,SNR);
-    [z_p,~]     = pytch_solver(x,[],1,SNR);
+    [z_r,~]     = rana_solver(x,[],1,SNR,0);
 
-    subplot(length(pp),2,length(pp)*(t-1)+1),plot(time*1e15,abs(x),time*1e15,abs(z_s),time*1e15,abs(z_p)),...
+    subplot(length(pp),2,length(pp)*(t-1)+1),plot(time*1e15,abs(x),time*1e15,abs(z_s),time*1e15,abs(z_r)),...
         title('Reconstructed magnitud'), xlabel('Time [fsec]','FontSize',16); ylabel('Amplitude','FontSize',16);
     subplot(length(pp),2,length(pp)*(t-1)+2),plot(time*1e15,unwrap(angle(x)),time*1e15,unwrap(angle(z_s)),...
-        time*1e15,unwrap(angle(z_p))), title('Reconstructed phase'),xlabel('Time [fsec]','FontSize',16); ylabel('Phase','FontSize',16);
+        time*1e15,unwrap(angle(z_r))), title('Reconstructed phase'),xlabel('Time [fsec]','FontSize',16); ylabel('Phase','FontSize',16);
 end
 
 %% Fig. 2
@@ -33,8 +31,10 @@ for t=1:length(L)
     x = pulse_set(2,:).';
     [z_s,~,~,A1] = smoothing_solver(x,[],L(t),SNR);
     [z_p,~]      = pytch_solver(x,[],L(t),SNR);
+    [z_r,~]      = rana_solver(x,[],L(t),SNR,1);
 
     subplot(length(L),3,length(L)*(t-1)+1),imagesc(abs(A1(z_p))),title('Pytch'),xlabel('Time [fsec]','FontSize',16);
+    subplot(length(L),3,length(L)*(t-1)+2),imagesc(abs(A1(z_r))),title('RANA'),xlabel('Time [fsec]','FontSize',16);
     subplot(length(L),3,length(L)*(t-1)+3),imagesc(abs(A1(z_s))),title('Proposed'),xlabel('Time [fsec]','FontSize',16);
 end
 %% Fig. 3
@@ -45,11 +45,12 @@ for t=1:length(L)
     x = pulse_set(2,:).';
     [z_s,~,~,A1] = smoothing_solver(x,[],L(t),SNR);
     [z_p,~]      = pytch_solver(x,[],L(t),SNR);
+    [z_r,~]      = rana_solver(x,[],L(t),SNR,1);
 
-    subplot(length(L),2,2*(t-1)+1),plot(time*1e15,abs(x),time*1e15,abs(z_s),time*1e15,abs(z_p)),...
+    subplot(length(L),2,2*(t-1)+1),plot(time*1e15,abs(x),time*1e15,abs(z_s),time*1e15,abs(z_p),time*1e15,abs(z_r)),...
         title('Reconstructed magnitud'),xlabel('Time [fsec]','FontSize',16); ylabel('Amplitude','FontSize',16);
     subplot(length(L),2,2*(t-1)+2),plot(time*1e15,unwrap(angle(x)),time*1e15,unwrap(angle(z_s)),...
-        time*1e15,unwrap(angle(z_p))), title('Reconstructed phase'),xlabel('Time [fsec]','FontSize',16); ylabel('Phase','FontSize',16);
+        time*1e15,unwrap(angle(z_p)),time*1e15,unwrap(angle(z_r))), title('Reconstructed phase'),xlabel('Time [fsec]','FontSize',16); ylabel('Phase','FontSize',16);
 end
 
 %% Fig. 4
@@ -57,6 +58,7 @@ L   = 1:1:6;
 SNR = 0;
 prob_s = zeros(12,6);
 prob_p = zeros(12,6);
+prob_r = zeros(12,6);
 prob_pg = zeros(12,6);
 c = 0;
 
@@ -68,6 +70,7 @@ for t=1:length(L)
             x0  = x + del*randsrc(1,1,[-1,1]);
             [~,error_s,~,~] = smoothing_solver(x,x0,L(t),SNR);
             [~,error_p]     = pytch_solver(x,x0,L(t),SNR);
+            [~,error_r]     = rana_solver(x,x0,L(t),SNR,0);
             [~,error_pg]    = PCPG_sol(x,x0,L(t),SNR);
 
             if min(error_s(error_s>0))<=1e-6
@@ -75,6 +78,9 @@ for t=1:length(L)
             end
             if min(error_p(error_p>0))<=1e-6
                 prob_p(c,t) = prob_p(c,t) + 1;
+            end
+            if min(error_p(error_r>0))<=1e-6
+                prob_r(c,t) = prob_r(c,t) + 1;
             end
             if min(error_p(error_pg>0))<=1e-6
                 prob_pg(c,t) = prob_pg(c,t) + 1;
@@ -89,6 +95,8 @@ subplot(2,2,2*(t-1)+1),imagesc(prob_s/100),colormap gray,title('Proposed'),...
     xlabel('L','FontSize',16); ylabel('delta','FontSize',16);
 subplot(2,2,2*(t-1)+2),imagesc(prob_p/100),colormap gray,title('Pytch'),...
     xlabel('L','FontSize',16); ylabel('delta','FontSize',16);
+subplot(2,2,2*(t-1)+3),imagesc(prob_r/100),colormap gray,title('RANA'),...
+    xlabel('L','FontSize',16); ylabel('delta','FontSize',16);
 subplot(2,2,2*(t-1)+4),imagesc(prob_pg/100),colormap gray,title('PCPG'),...
     xlabel('L','FontSize',16); ylabel('delta','FontSize',16);
 
@@ -97,13 +105,16 @@ L   = 1:1:8;
 SNR = 0;
 ferror_s = zeros(length(L),1);
 ferror_p = zeros(length(L),1);
+ferror_r = zeros(length(L),1);
 
 for t=1:length(L)
     x = pulse_set(2,:).';
     for it=1:100
         [~,~,error_s,error_p] = smoothing_init(x,L(t),SNR);
+        [~,error_r]           = proc_rana(x,L(t),SNR,0);
         ferror_s(t) = ferror_s(t) + error_s;
         ferror_p(t) = ferror_p(t) + error_p;
+        ferror_r(t) = ferror_r(t) + error_r;
     end
     fprintf('L = %d\n',t);
 end
@@ -115,18 +126,18 @@ L   = 4;
 SNR = 0;
 x   = pulse_set(2,:).';
 [z_s,z_p,~,~] = smoothing_init(x,L,SNR);
+[z_r,~]       = proc_rana(x,L,SNR,0);
 
 [z_s,~,~,~] = smoothing_solver(x,z_s,L,SNR);
 [z_p,~,~,~] = smoothing_solver(x,z_p,L,SNR);
 [z_r,~,~,~] = smoothing_solver(x,z_r,L,SNR);
 
-subplot(1,2,1),plot(time*1e15,abs(x),time*1e15,abs(z_s),time*1e15,abs(z_p)),title('Reconstructed magnitud'),...
+subplot(1,2,1),plot(time*1e15,abs(x),time*1e15,abs(z_s),time*1e15,abs(z_p),time*1e15,abs(z_r)),title('Reconstructed magnitud'),...
     xlabel('Time [fsec]','FontSize',16); ylabel('Amplitude','FontSize',16);
-subplot(1,2,2),plot(time*1e15,unwrap(angle(x)),time*1e15,unwrap(angle(z_s)), time*1e15,unwrap(angle(z_p))),...
+subplot(1,2,2),plot(time*1e15,unwrap(angle(x)),time*1e15,unwrap(angle(z_s)), time*1e15,unwrap(angle(z_p)),time*1e15,unwrap(angle(z_r))),...
     title('Reconstructed phase'),xlabel('Time [fsec]','FontSize',16); ylabel('Phase','FontSize',16);
 
 %% Fig. 7
-
 L   = 1:1:8;
 SNR = 8:4:20;
 ferror_s = zeros(length(SNR),length(L));
