f3337244a21954c9bb67f03ae36f596dbed1f775
samuelpinilla <47015971+samuelpinilla@users.noreply.github.com>
2019-03-08 19:16:42 -0500

    Update init_rana.m

--- results/rana/init_rana.m
+++ results/rana/init_rana.m
@@ -1,47 +1,75 @@
-function [z,er] = rana_solver(x,x0,L,SNR)
+function x1 = init_rana(Y,x)
+%% initialization
+N  = size(Y,1);
+g  = sum(Y,2);
+
+s = sqrt(ifft(ifftshift(g)));
+s = s/norm(s,'fro');
+
 %% variables
-[N,~] = size(x);
+alpha = 0.09;
+beta  = 0.425;
+gamma = 1;
 
-A  = @(I) fftshift(fft(FROG_signal(I,1,N)),1);       % propagation operator
+t0 = randsrc(1,1,3:N-3);
+fprintf('t0=%d\n',t0);
+ll = t0+1:1:N;
 
-%% Make signal and data (noiseless)
-
-y     = abs(A(x));
-ind_L = 1:L:N;
-
-if SNR >0
-    aux = awgn(y,SNR,'measured');
-    Ynoisy = zeros(size(y));
-    Ynoisy(:,ind_L) = aux(:,ind_L);
-else
-    Ynoisy = y;
+for r=1:length(ll)
+    t = ll(r);
+    
+    term1p = alpha*abs(s(t)-s(t-1))^2;
+    term1n = alpha*abs(-s(t)-s(t-1))^2;
+    
+    term2p = beta*abs(s(t)-s(t-1)-(s(t-1)-s(t-2)))^2;
+    term2n = beta*abs(-s(t)-s(t-1)-(s(t-1)-s(t-2)))^2;
+        
+    term3p = gamma*abs(s(t)-s(t-1)-(s(t-1)-s(t-2)) - (s(t-1)-s(t-2)-(s(t-2)-s(t-3))))^2;
+    term3n = gamma*abs(-s(t)-s(t-1)-(s(t-1)-s(t-2)) - (s(t-1)-s(t-2)-(s(t-2)-s(t-3))))^2;
+    
+    epsilon1 = term1p + term2p + term3p;
+    epsilon2 = term1n + term2n + term3n;
+    
+    if epsilon1 > epsilon2
+        s(t) = -s(t);
+    end
 end
 
-if isempty(x0)
-    z  = init_rana(Ynoisy.^2);
-    z  = z.';
-else
-    z = x0;
+ll = t0-1:-1:1;
+
+for r=1:length(ll)
+    t = ll(r);
+    
+    term1p = alpha*abs(s(t)-s(t+1))^2;
+    term1n = alpha*abs(-s(t)-s(t+1))^2;
+    
+    term2p = beta*abs(s(t)-s(t+1)-(s(t+1)-s(t+2)))^2;
+    term2n = beta*abs(-s(t)-s(t+1)-(s(t+1)-s(t+2)))^2;
+        
+    term3p = gamma*abs(s(t)-s(t+1)-(s(t+1)-s(t+2)) - (s(t+1)-s(t+2)-(s(t+2)-s(t+3))))^2;
+    term3n = gamma*abs(-s(t)-s(t+1)-(s(t+1)-s(t+2)) - (s(t+1)-s(t+2)-(s(t+2)-s(t+3))))^2;
+        
+    epsilon1 = term1p + term2p + term3p;
+    epsilon2 = term1n + term2n + term3n;
+    
+    if epsilon1 > epsilon2
+        s(t) = -s(t);
+    end
 end
 
-error   = norm(y-abs(A(z)),'fro')/norm(y,'fro');
-fprintf('Iter = 0  Error = %f \n', error);
+%% estimated spectrum
 
-z = z.';
+x0 = sqrt(fftshift(fft(s)/sqrt(N))).*exp(-1i*randsrc(N,1,[0,1]));
+% x0 = x0/norm(x0);
 
-tic
-[~,z,er] = ispecshg(Ynoisy,z,z,A,300);
-toc
+%% real spectrum
+% Re = fftshift(abs(fft(x)))/sqrt(N);
 
-z  = best_sol(z.', x);
+% error = norm(abs(x0)-Re)/N;
+% fprintf('spectrum estimation: %f\n',error);
 
-%% plots
-% T = length(er);
-% 
-% figure;
-% subplot(2,2,1);imagesc(y),title('Simulated trace');
-% subplot(2,2,2);imagesc(abs(A(z))),title('Reconstructed trace');
-% subplot(2,2,3);plot(time,abs(x),time,abs(z)),title('Reconstructed amplitude');
-% subplot(2,2,4);plot(time*1e15,unwrap(angle(x))/pi,time*1e15,unwrap(angle(z))/pi),title('Reconstructed phase');
+% figure;plot(1:N,abs(x0)/norm(x0),1:N,Re);
 
+x1 = ifft(ifftshift(x0))*sqrt(N);
+x1 = x1/norm(x1,'fro');
 end
