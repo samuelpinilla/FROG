b04fb59b11187003e9cce7d8ea725669c9215455
samuelpinilla <47015971+samuelpinilla@users.noreply.github.com>
2019-04-08 23:15:35 -0500

    Add files via upload

---
+++ aux.m
@@ -0,0 +1,17 @@
+function y = aux(x,L,N,fi)
+    T    = 100;
+    time = linspace(-T,T,N).';
+    time = time*1e-15;
+    dt   = time(2)-time(1);
+    F    = (-N/2:N/2-1).';
+    F    =  fftshift( F/dt/N );
+    ind  = 1:N; 
+    D    = time(ind).';
+
+    y    = zeros(N,length(fi));
+    X    = fft(x);
+
+    for ik=1:size(y,2)
+        y(:,ik) = ifft( X.*exp(1i*2*pi*D((fi(ik)-1)*L+1)*F) ) + ifft( X.*exp(-1i*2*pi*D((fi(ik)-1)*L+1)*F).*exp(1i*2*pi*D((fi(ik)-1)*L+1)*F(fi(ik))) );
+    end
+end
